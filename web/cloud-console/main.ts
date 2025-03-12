2651e8bd4f0033b84d0c921aaf9a4c428711ffe7
Benjamin Benetti <ben@bbenetti.ca>
2025-03-11 21:33:03 -0700

    working lib

--- web/cloud-console/main.ts
+++ web/cloud-console/main.ts
@@ -1,8 +1,6 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import { add } from "@bcloud/web-common";
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  console.log("Add 2 + 3 =", add(2, 3));  
 }
