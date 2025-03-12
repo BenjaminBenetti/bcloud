90a50eb50e443315f765503fc8b00546f3931b22
Benjamin Benetti <ben@bbenetti.ca>
2025-03-11 22:07:39 -0700

    get deno and react working in a workspace project. darn that tricky node modules manual thing!

--- web/cloud-console/main.ts
+++
@@ -1,6 +0,0 @@
-import { add } from "@bcloud/web-common";
-
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));  
-}
