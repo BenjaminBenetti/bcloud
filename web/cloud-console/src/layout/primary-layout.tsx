0f3b1f861c311aacd619c06c77c26c3dc7a3c529
Benjamin Benetti <ben@bbenetti.ca>
2025-03-12 20:06:33 -0700

    install react router and get it working!

---
+++ web/cloud-console/src/layout/primary-layout.tsx
@@ -0,0 +1,9 @@
+import { Outlet } from "react-router";
+
+export function PrimaryLayout() {
+  return (
+    <div>
+      <Outlet></Outlet>
+    </div>
+  );
+}
--- web/cloud-console/src/main.tsx
+++ web/cloud-console/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { PrimaryLayout } from "./layout/primary-layout.tsx";
+import { DashboardOverview } from "./pages/dashboard/dashboard-overview.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PrimaryLayout />}>
+          <Route index element={<DashboardOverview />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
