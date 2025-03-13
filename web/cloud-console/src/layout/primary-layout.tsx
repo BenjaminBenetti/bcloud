07834d836c5e1e51aa4eb48bacdb31d302f661bc
Benjamin Benetti <ben@bbenetti.ca>
2025-03-12 20:45:57 -0700

    fix react class name, turns out you cant use just 'class' lol

--- web/cloud-console/src/layout/primary-layout.tsx
+++ web/cloud-console/src/layout/primary-layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 
 export function PrimaryLayout() {
   return (
-    <div class="bg-emerald-500 h-screen w-screen">
+    <div className="bg-emerald-500 h-screen w-screen">
       <Outlet></Outlet>
     </div>
   );
